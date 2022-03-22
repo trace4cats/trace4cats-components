7e0a7a4247542c82b9a8c1f73b76edef77633c98
Scala Steward <me@scala-steward.org>
2022-03-22 04:39:23 +0100

    Update trace4cats-avro-kafka-consumer, ... to 0.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,16 +2,16 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val trace4cats = "0.12.0"
-    val trace4catsAvro = "0.12.0"
-    val trace4catsAvroKafka = "0.12.0"
-    val trace4catsDatadog = "0.12.0"
-    val trace4catsJaeger = "0.12.0"
-    val trace4catsOpentelemetry = "0.12.0"
-    val trace4catsCloudtrace = "0.12.0"
-    val trace4catsNewrelic = "0.12.0"
-    val trace4catsZipkin = "0.12.0"
-    val trace4catsTailSamplingExtras = "0.12.0"
+    val trace4cats = "0.13.0"
+    val trace4catsAvro = "0.13.0"
+    val trace4catsAvroKafka = "0.13.0"
+    val trace4catsDatadog = "0.13.0"
+    val trace4catsJaeger = "0.13.0"
+    val trace4catsOpentelemetry = "0.13.0"
+    val trace4catsCloudtrace = "0.13.0"
+    val trace4catsNewrelic = "0.13.0"
+    val trace4catsZipkin = "0.13.0"
+    val trace4catsTailSamplingExtras = "0.13.0"
 
     val circe = "0.14.1"
     val circeYaml = "0.14.1"
