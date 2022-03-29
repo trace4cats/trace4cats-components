37d915098457272d225dfea1bec216774eb3a80b
Scala Steward <me@scala-steward.org>
2022-03-30 01:12:02 +0200

    Update grpc-okhttp to 1.45.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     val circeYaml = "0.14.1"
     val decline = "2.2.0"
     val graalKafkaClient = "0.1.0"
-    val grpc = "1.45.0"
+    val grpc = "1.45.1"
     val http4sJdkClient = "0.7.0"
     val log4cats = "2.2.0"
     val logback = "1.2.11"
