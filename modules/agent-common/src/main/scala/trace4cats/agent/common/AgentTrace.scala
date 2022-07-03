0803c39c95401cc718f81f5e0379c6e8227120e2
Chris Jansen <janstenpickle@users.noreply.github.com>
2022-07-03 14:43:06 +0100

    Reorganise packages

---
+++ modules/agent-common/src/main/scala/trace4cats/agent/common/AgentTrace.scala
@@ -0,0 +1,34 @@
+package trace4cats.agent.common
+
+import java.net.InetAddress
+
+import cats.effect.kernel.{Async, Resource, Sync}
+import fs2.{Chunk, Pipe}
+import trace4cats.RateSpanSampler
+import trace4cats.kernel.{SpanExporter, SpanSampler}
+import trace4cats.meta.{PipeTracer, TracedSpanExporter}
+import trace4cats.model.{AttributeValue, CompletedSpan, TraceProcess}
+
+object AgentTrace {
+  def apply[F[_]: Async](
+    exporterName: String,
+    exporterAttributes: Map[String, AttributeValue],
+    listenerPort: Int,
+    sampleRate: Option[Double],
+    bufferSize: Int,
+    exporter: SpanExporter[F, Chunk]
+  ): Resource[F, (Pipe[F, CompletedSpan, CompletedSpan], SpanExporter[F, Chunk])] = for {
+    hostname <- Resource.eval(Sync[F].blocking(InetAddress.getLocalHost.getHostName))
+    process = TraceProcess("trace4cats-agent", Map("hostname" -> hostname))
+
+    sampler <- sampleRate.fold(Resource.pure[F, SpanSampler[F]](SpanSampler.always[F]))(rate =>
+      RateSpanSampler.create[F](bufferSize, rate)
+    )
+
+    pipe = PipeTracer[F](
+      Map("listen.protocol" -> "udp", "listen.format" -> "avro", "listen.port" -> listenerPort),
+      process,
+      sampler
+    )
+  } yield pipe -> TracedSpanExporter[F](exporterName, exporterAttributes, process, sampler, exporter)
+}
---
+++ modules/collector-common/src/main/scala/trace4cats/collector/common/config/package.scala
@@ -0,0 +1,8 @@
+package trace4cats.collector.common
+
+import io.circe.generic.extras.Configuration
+
+package object config {
+  implicit val circeDecoderConfig: Configuration =
+    Configuration.default.withDefaults.withKebabCaseMemberNames.withKebabCaseConstructorNames
+}
