9c1738e01b1dd0d372a3fffdeb551952c90ef027
Scala Steward <scala_steward@virtuslab.com>
2022-07-27 18:45:39 +0000

    Update cats-effect to 3.3.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val trace4catsZipkin = "0.13.1+17-1553c1c6"
     val trace4catsTailSamplingExtras = "0.13.1+16-f4b6dc0d"
 
-    val catsEffect = "3.3.12"
+    val catsEffect = "3.3.14"
     val circe = "0.14.1"
     val circeYaml = "0.14.1"
     val decline = "2.3.0"
