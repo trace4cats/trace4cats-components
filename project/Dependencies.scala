7252b8ed03aadab5138dae6f7e4291e937c57f75
Scala Steward <scala_steward@virtuslab.com>
2023-06-05 06:19:09 +0000

    Update trace4cats-opentelemetry-jaeger-exporter, ... to 0.14.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
 
     val trace4catsJaeger = "0.14.0"
 
-    val trace4catsOpentelemetry = "0.14.0"
+    val trace4catsOpentelemetry = "0.14.1"
 
     val trace4catsCloudtrace = "0.14.0"
 
